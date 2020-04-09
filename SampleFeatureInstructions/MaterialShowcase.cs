2630f349a1b824ef07d1fe958efe88f81584d765
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2020-04-09 15:59:28 +0530

    Add files via upload

---
+++ SampleFeatureInstructions/MaterialShowcase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace SampleFeatureInstructions
+{
+    public class MaterialShowcase : UIView
+    {
+        // MARK: Material design guideline constant
+        public const float BackgroundAlpha = .96f;
+        public const float TargetHolderRadius = 44f;
+        public const float TextCenterOffset = 44f + 20f;
+        public const float InstructionsCenterOffset = 20f;
+        public const float LabelMargin = 40f;
+        public const float TargetPadding = 20f;
+
+        // Other default properties
+        public const float LabelDefaultHeight = 50f;
+        public static UIColor BackgroundDefaultColor = UIColor.Black;
+        public static UIColor TargetHolderColor = UIColor.Yellow;
+
+        // MARK: Animation properties
+        public const float AniComeInDuration = .5f; // second
+        public const float AniGoOutDuration = .5f;  // second
+        public const float AniTargetHolderScale = 2.2f;
+        public static UIColor AniRippleColor = UIColor.Green;
+        public const float AniRippleAlpha = .5f;
+        public const float AniRippleScale = 1.6f;
+
+        public const float OffsetThreshold = 88f;
+
+        // MARK: Private view properties
+        public UIView containerView;
+        public UIView targetView;
+        public UIView backgroundView;
+        public UIView targetHolderView;
+        public UIView hiddenTargetHolderView;
+        public UIView targetRippleView;
+        public UIView targetCopyView;
+        public MaterialShowcaseInstructionView instructionView;
+
+        // MARK: Public Properties
+
+        // Background
+        public UIColor backgroundPromptColor = UIColor.Blue;
+        public float backgroundPromptColorAlpha;
+        // Target
+        public bool shouldSetTintColor = true;
+        public UIColor targetTintColor;
+        public float targetHolderRadius;
+        public UIColor targetHolderColor;
+        // Text
+        public string primaryText;
+        public string secondaryText;
+        public UIColor primaryTextColor;
+        public UIColor secondaryTextColor;
+        public float primaryTextSize;
+        public float secondaryTextSize;
+        public UIFont primaryTextFont;
+        public UIFont secondaryTextFont;
+        // Animation
+        public float aniComeInDuration;
+        public float aniGoOutDuration;
+        public float aniRippleScale;
+        public UIColor aniRippleColor;
+        public float aniRippleAlpha;
+
+        public MaterialShowcase() : base(new CGRect(0, 0, UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height))
+        {
+            this.Configure();
+        }
+    }
+}
---
+++ SampleFeatureInstructions/ViewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UIKit;
+
+namespace SampleFeatureInstructions
+{
+    public partial class ViewController : UIViewController
+    {
+        private UIBarButtonItem target;
+
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.Title = "Features";
+
+            target = new UIBarButtonItem(UIBarButtonSystemItem.Add, (sender, args) =>
+            {
+                Console.WriteLine("bar button clicked");
+
+                // Instantiate a new MaterialShowcase
+                var showcase = new MaterialShowcase();
+                showcase.primaryText = "Hello";
+                showcase.secondaryText = "I'm a showcase";
+
+                showcase.backgroundPromptColorAlpha = 0.96f;
+
+                showcase.targetTintColor = UIColor.Blue;
+                showcase.targetHolderRadius = 44;
+                showcase.targetHolderColor = UIColor.White;
+
+                showcase.primaryTextColor = UIColor.White;
+                showcase.secondaryTextColor = UIColor.White;
+                showcase.primaryTextSize = 20;
+                showcase.secondaryTextSize = 15;
+                showcase.primaryTextFont = UIFont.BoldSystemFontOfSize(20);
+                showcase.secondaryTextFont = UIFont.SystemFontOfSize(15);
+
+                showcase.aniComeInDuration = 0.5f;
+                showcase.aniGoOutDuration = 0.5f;
+                showcase.aniRippleScale = 1.5f;
+                showcase.aniRippleColor = UIColor.White;
+                showcase.aniRippleAlpha = 0.2f;
+
+                showcase.backgroundPromptColor = UIColor.Blue;
+
+                showcase.SetTargetView(target);
+                showcase.containerView = UIApplication.SharedApplication.KeyWindow;
+
+                showcase.Show();
+            });
+
+            NavigationItem.SetRightBarButtonItem(target, true);
+        }
+    }
+}
